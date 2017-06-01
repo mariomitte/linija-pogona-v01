66ed48930785f87f11ada2e69934c7664e80a1af
mariomitte <mario_mitte@hotmail.com>
2017-06-01 19:14:18 +0200

    Uvedena uvodna stranica o projektu

---
+++ postavi.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+#
+# Postavljanje RPi modela 3 za projekt "Linija pogona"
+#
+#
+
+echo "\n"
+echo "Prvo provjeri nadogradnje\n"
+sudo apt-get -y update
+
+echo "\n"
+echo "Instalacija Python biblioteka\n"
+sudo apt-get install -y build-essential python-dev
+sudo apt-get install -y python3-pip
+sudo apt-get install -y openssh-server
+sudo apt-get install -y python3-rpi.gpio
+sudo apt-get install -y python3-picamera
+
+echo "sada Django\n"
+sudo pip3 install virtualenv
+echo "\n"
+echo "Direktorij za venv"
+virertualenv myenv
+virtualenv -p /usr/bin/python3 myenv
+source myenv/bin/activate
+echo "\n"
+echo "Instaliraj potrebne biblioteke za rad sa Django-om u venv"
+pip3 install -r requirements.txt
+deactivate
+
+echo "\n"
+echo "Kraj skripte."
